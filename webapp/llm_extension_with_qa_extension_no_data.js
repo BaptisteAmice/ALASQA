27c5bd6071f0095b450eac280c87c58fe5bb993c
Amice Baptiste <amice.bap@gmail.com>
2025-02-11 11:43:16 +0100

    llm extension with qa extension extracteur/raisonneur v1

--- webapp/llm_extension_with_qa_extension_no_data.js
+++ webapp/llm_extension_with_qa_extension_no_data.js
@@ -14,14 +14,21 @@ window.addEventListener(
 });
 
 async function qa_control() {
-    let input_field = document.getElementById("user-input");
-    input = input_field.value;
-    let responseTextBalise = document.getElementById("response");
-    systemMessage = "Your task is to build commands to query the knowledge graph to find data that answers the given question. Several successive commands can be used and should be separated by a semicolon. For example, to find the parent of Einstein, you can use the following reasoning and commands: \n <think>Einstein is a person, and his parents are the people who have him as a child</think>. \n<commands>a person; has child; Albert Einstein</commands>. \n As in the example, you don't need to anwer to the question but to make a quick reasonning about which kind of entity you need to find in the graphe and then construct a command in the same format to find the corresponding data in the knowledge graph.";
+    //reset sparklis
+    sparklis.home();
 
+    let input_field = document.getElementById("user-input");
+    let input_question = input_field.value;
+    let responseTextBalise = document.getElementById("response");
+    let responseTextBalise2 = document.getElementById("response2");
+
+    let systemMessage = "Your task is to build commands to query the knowledge graph to find data that answers the given question. Several successive commands can be used and should be separated by a semicolon. For example, to find the parent of Einstein, you can use the following reasoning and commands: \n <think>Einstein is a person, and his parents are the people who have him as a child</think>. \n<commands>a person; has child; Albert Einstein</commands>.\n Another example with the question 'Which animal is from the camelini family?':<think>I need to find ANIMALS, that are members of a FAMILY, and this family needs t be camelini.</think><commands>a animal ; has family ; camelini</commands> \n As in the examples, you don't need to anwer to the question but to make a quick reasonning about which kind of entity you need to find in the graphe and then construct a command in the same format to find the corresponding data in the knowledge graph.";
+    let userMessage;
+
+    ////EXTRACTION
    
-    //let output = await sendPrompt(usualPrompt(systemMessage, input), streamOption = true, outputField = responseTextBalise);
-    let output = 'blablabla<commands>a animal ;</commands>dsfsd';
+    let output = await sendPrompt(usualPrompt(systemMessage, input_question), streamOption = true, outputField = responseTextBalise);
+    //let output = 'blablabla<commands>a animal ; has family ; camelini</commands>dsfsd';
     //get commands from regular expression <commands>...</commands>
     let match = output.match(/<commands>(.*?)<\/commands>/s);
     
@@ -32,17 +39,43 @@ async function qa_control() {
         qa.value = commands;  // Safe access since we checked if commands is not null
     } else {
         console.error("No match found for <commands>...</commands>");
+        qa.value = "Error";
+        return;
     }
 
+    //wait for the endpoint to be ready //todo plus élégamment
+    await new Promise(r => setTimeout(r, 2000));
 
     //Execute commands
     let event = new KeyboardEvent('keyup', { 'keyCode': 13 });
     qa.dispatchEvent(event);
+
+    //wait for the endpoint to be ready //todo plus élégamment
+    await new Promise(r => setTimeout(r, 2000));
     
-    //get sparklis results from the commands //todo
-    let sparql = p.sparql();
-    console.log(sparql);
-    //console.log(sparklis.evalSparql(sparql));
-    //p.sparql(): string
-    //sparklis.evalSparql(query: string): Promise(sparklis-results, int)
+    //get sparklis results from the commands
+    let place = sparklis.currentPlace();
+
+    place.onEvaluated(async () => {
+        console.log("evaluated");
+        let results = place.results();
+        console.log(results);
+        console.log('rows',results.rows);
+        let rows = results.rows;
+        let resultText = rows.map(item => item[0].uri).join(', '); //todo check
+        console.log("result",resultText);
+
+
+        //todo check error avant prompt 2//if output ="" -> no result
+        //todo prompt 2
+
+        ////REASONING
+
+        let systemMessage2 = "Your task was to build commands to query the knowledge graph to find data that answers the given question. The question will be repeated to you, with the results you found. Use them to answer the question. Just write the answer and nothing else.";
+        let userMessage2 = input_question + " : " + resultText;
+        //todo très lent et résultat très mauvais
+        let output2 = await sendPrompt(usualPrompt(systemMessage2, userMessage2), streamOption = true, outputField = responseTextBalise2);
+        console.log("output",output);
+    });    
+    
 }
