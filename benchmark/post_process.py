c6ad2db910356989699cd7db671a0a450f600932
Amice Baptiste <amice.bap@gmail.com>
2025-03-05 11:25:00 +0100

    generating pdf from post-processing

--- benchmark/post_process.py
+++ benchmark/post_process.py
@@ -2,8 +2,14 @@ from collections import defaultdict
 import json
 import logging
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import os
 import re
+
+
+# Page to display all data
+pp = PdfPages("post_process_data.pdf")
+show = False
 
 command_list = [
     "a",
@@ -46,6 +52,7 @@ def precision_recall_f1_plot(precision, recall, f1_scores):
     Plot the precision, recall and f1 scores for each question.
     """
     questions = range(1, len(precision) + 1)  # Adjust based on your data length
+    plt.figure()
     plt.scatter(questions, precision, label="Precision", marker="o")
     plt.scatter(questions, recall, label="Recall", marker="s")
     plt.scatter(questions, f1_scores, label="F1-score", marker="^")
@@ -55,21 +62,26 @@ def precision_recall_f1_plot(precision, recall, f1_scores):
     plt.title("Performance of the system for each question")
     plt.legend()
     plt.grid(True)
-    plt.show() 
+    pp.savefig()
+    if show:
+        plt.show() 
 
 # Boxplot of the scores
-def boxplot_scores(precision, recall, f1_scores, title: str = "Boxplot of the scores"):
+def boxplot_scores(precision, recall, f1_scores, title_end: str):
     """
     Boxplot of the scores.
     """
+    plt.figure()
     plt.boxplot([precision, recall, f1_scores], 
                 tick_labels=[ "Precision \n(Mean: {:.2f})".format(sum(precision) / len(precision)),
                               "Recall \n(Mean: {:.2f})".format(sum(recall) / len(recall)),
                               "F1-score \n(Mean: {:.2f})".format(sum(f1_scores) / len(f1_scores))])
     plt.ylabel("Score")
-    plt.title(title)
+    plt.title("Boxplot of the scores for " + title_end)
     plt.grid(True)
-    plt.show()
+    pp.savefig()
+    if show:
+        plt.show()
 
 import json
 from collections import defaultdict
@@ -145,7 +157,7 @@ def count_metric_values(filtered_data):
 import matplotlib.pyplot as plt
 import numpy as np
 
-def hist_first_non_done_step(data: list, title: str = "Histogram of First Non-Done Steps"):
+def hist_first_non_done_step(data: list, title_end: str):
     # Convert None to a string for proper sorting
     filtered_data = ['None' if x is None else x for x in data]
     
@@ -156,12 +168,15 @@ def hist_first_non_done_step(data: list, title: str = "Histogram of First Non-Do
     unique_values = sorted(unique_values, key=lambda x: (x == 'None', x))
     
     # Plot histogram
+    plt.figure()
     plt.bar(unique_values, counts, color='skyblue', edgecolor='black')
     plt.xlabel('First non-Done Step')
     plt.ylabel('Count')
-    plt.title(title)
+    plt.title("Histogram of First Non-Done Steps for "  + title_end)
     plt.xticks(unique_values)  # Ensure all unique values appear on x-axis
-    plt.show()
+    pp.savefig()
+    if show:
+        plt.show()
 
 def find_first_non_done_step(filtered_data):
     """
@@ -198,11 +213,14 @@ def plot_box_system_time(filtered_data):
     Plot the boxplot of the SystemTime.
     """
     system_times = [entry.get("SystemTime") for entry in filtered_data]
+    plt.figure()
     plt.boxplot(system_times)
     plt.ylabel("System Time")
     plt.title("Boxplot of the System Time")
     plt.grid(True)
-    plt.show()
+    pp.savefig()
+    if show:
+        plt.show()
 
 def get_commands_from_reasoning(reasoning: str) -> list:
     """
@@ -306,7 +324,7 @@ def plot_command_boxplots(used_commands_lists, scores):
             means.append(np.mean(cmd_scores))  # Calculate mean for each command
 
     # Step 4: Plot the Box Plot
-    plt.figure(figsize=(12, 6))
+    plt.figure()
     plt.boxplot(boxplot_data, labels=boxplot_labels)
 
     # Overlay mean values
@@ -322,22 +340,30 @@ def plot_command_boxplots(used_commands_lists, scores):
 
     # Add legend for the mean marker
     plt.legend()
-    plt.show()
+    pp.savefig()
+    if show:
+        plt.show()
 
 def all_prints(file_name: str):
+    # Data to be displayed in a table
+    table_headers = []
+    table_data = []
+
     # Unvalid data
     constraints_invalid = {
         "BenchmarkResult": lambda x : x is None or x == []
     }
     filtered_invalid_data = load_and_filter_data(file_name, constraints_invalid)
-    print("Number of invalid data:", len(filtered_invalid_data))
-    
+    table_headers.append("Empty BenchmarkResult")
+    table_data.append([len(filtered_invalid_data)])
+        
     # Valid data
     constraints_none = {
         "BenchmarkResult": lambda x : x not in [None, []]
     }   
     filtered_valid_data = load_and_filter_data(file_name, constraints_none)
-    print("Number of valid data:", len(filtered_valid_data))
+    table_headers.append("Valid Data")
+    table_data.append([len(filtered_valid_data)])
 
     # Commands
     commands_list = get_commands_list(filtered_valid_data)
@@ -347,81 +373,39 @@ def all_prints(file_name: str):
 
     precisions, recalls, f1_scores = extract_scores(filtered_valid_data)
     precision_recall_f1_plot(precisions, recalls, f1_scores)
-    boxplot_scores(precisions, recalls, f1_scores, "Boxplot of the scores for valid data")
+    boxplot_scores(precisions, recalls, f1_scores, "valid data")
 
     plot_box_system_time(filtered_valid_data)
 
     non_done_step = find_first_non_done_step(filtered_valid_data)
-    hist_first_non_done_step(non_done_step, "Histogram of First Non-Done Steps for Valid Data")
+    hist_first_non_done_step(non_done_step, "Valid Data")
 
     # Empty reasoning
     constraints_empty_reasoning = {
         "Reasoning": lambda x: x == ""
     }
     filtered_empty_reasoning_data = load_and_filter_data(file_name, constraints_empty_reasoning)
-    print("Number of empty reasoning:", len(filtered_empty_reasoning_data))
+    table_headers.append("Empty Reasoning")
+    table_data.append([len(filtered_empty_reasoning_data)])    
 
     # Empty SystemResult
     constraints_empty_system_result = {
         "SystemResult": lambda x: x == "" or x is None or x == []
     }
     filtered_empty_system_result_data = load_and_filter_data(file_name, constraints_empty_system_result)
-    print("Number of empty SystemResult:", len(filtered_empty_system_result_data))
+    table_headers.append("Empty SystemResult")
+    table_data.append([len(filtered_empty_system_result_data)])
 
     non_done_step = find_first_non_done_step(filtered_empty_system_result_data)
-    hist_first_non_done_step(non_done_step, "Histogram of First Non-Done Steps for Empty SystemResult")
-
-    #todo specific warnings (from alerts, etc.)
-
-    #todo specific used commands
-
-    #todo matrice etape premiere erreur / commande
-
-    # Expected boolean
-    constraints_expected_boolean = {
-        "BenchmarkResultType": lambda x: x == "boolean"
-    }
-    filtered_expected_boolean_data = load_and_filter_data(file_name, constraints_expected_boolean)
-    print("Number of expected boolean:", len(filtered_expected_boolean_data))
-
-    precisions, recalls, f1_scores = extract_scores(filtered_expected_boolean_data)
-    boxplot_scores(precisions, recalls, f1_scores, "Boxplot of the scores for Expected Boolean")
-
-    non_done_step = find_first_non_done_step(filtered_expected_boolean_data)
-    hist_first_non_done_step(non_done_step, "Histogram of First Non-Done Steps for Expected Boolean")
-
-    # Expected uri
-    constraints_expected_uri = {
-        "BenchmarkResultType": lambda x: x == "uri"
-    }
-    filtered_expected_uri_data = load_and_filter_data(file_name, constraints_expected_uri)
-    print("Number of expected uri:", len(filtered_expected_uri_data))
-
-    precisions, recalls, f1_scores = extract_scores(filtered_expected_uri_data)
-    boxplot_scores(precisions, recalls, f1_scores, "Boxplot of the scores for Expected URI")
-
-    non_done_step = find_first_non_done_step(filtered_expected_uri_data)
-    hist_first_non_done_step(non_done_step, "Histogram of First Non-Done Steps for Expected URI")
-
-    # Expected literal
-    constraints_expected_literal = {
-        "BenchmarkResultType": lambda x: x == "literal"
-    }
-    filtered_expected_literal_data = load_and_filter_data(file_name, constraints_expected_literal)
-    print("Number of expected literal:", len(filtered_expected_literal_data))
-
-    precisions, recalls, f1_scores = extract_scores(filtered_expected_literal_data)
-    boxplot_scores(precisions, recalls, f1_scores, "Boxplot of the scores for Expected Literal")
-
-    non_done_step = find_first_non_done_step(filtered_expected_literal_data)
-    hist_first_non_done_step(non_done_step, "Histogram of First Non-Done Steps for Expected Literal")
+    hist_first_non_done_step(non_done_step, "Empty SystemResult")
 
     # Non empty response from the system
     filtered_non_empty_response = {
         "SystemResult" : lambda x: x not in [None, "", []],
     }
     filtered_non_empty_response_data = load_and_filter_data(file_name, filtered_non_empty_response)
-    print("Number of non empty responses:", len(filtered_non_empty_response_data))
+    table_headers.append("Non Empty SystemResult")
+    table_data.append([len(filtered_non_empty_response_data)])
 
     # Non empty response from the system but with a score at  0
     filtered_non_empty_response_score_at_zero = {
@@ -429,11 +413,68 @@ def all_prints(file_name: str):
         "F1Score": lambda x: x == 0
     }
     filtered_non_empty_response_score_at_zero_data = load_and_filter_data(file_name, filtered_non_empty_response_score_at_zero)
-    print("Number of non empty responses with a score at 0:", len(filtered_non_empty_response_score_at_zero_data))
+    table_headers.append("Non Empty SystemResult (score at 0)")
+    table_data.append([len(filtered_non_empty_response_score_at_zero_data)])
 
+        # Expected boolean
+    constraints_expected_boolean = {
+        "BenchmarkResultType": lambda x: x == "boolean"
+    }
+    filtered_expected_boolean_data = load_and_filter_data(file_name, constraints_expected_boolean)
+    table_headers.append("Expected Boolean")
+    table_data.append([len(filtered_expected_boolean_data)])
 
+    precisions, recalls, f1_scores = extract_scores(filtered_expected_boolean_data)
+    boxplot_scores(precisions, recalls, f1_scores, "Expected Boolean")
+
+    non_done_step = find_first_non_done_step(filtered_expected_boolean_data)
+    hist_first_non_done_step(non_done_step, "Expected Boolean")
+
+    # Expected uri
+    constraints_expected_uri = {
+        "BenchmarkResultType": lambda x: x == "uri"
+    }
+    filtered_expected_uri_data = load_and_filter_data(file_name, constraints_expected_uri)
+    table_headers.append("Expected URI")
+    table_data.append([len(filtered_expected_uri_data)])
+
+    precisions, recalls, f1_scores = extract_scores(filtered_expected_uri_data)
+    boxplot_scores(precisions, recalls, f1_scores, "Expected URI")
+
+    non_done_step = find_first_non_done_step(filtered_expected_uri_data)
+    hist_first_non_done_step(non_done_step, "Expected URI")
+
+    # Expected literal
+    constraints_expected_literal = {
+        "BenchmarkResultType": lambda x: x == "literal"
+    }
+    filtered_expected_literal_data = load_and_filter_data(file_name, constraints_expected_literal)
+    table_headers.append("Expected Literal")
+    table_data.append([len(filtered_expected_literal_data)])
+
+    precisions, recalls, f1_scores = extract_scores(filtered_expected_literal_data)
+    boxplot_scores(precisions, recalls, f1_scores, "Expected Literal")
+
+    non_done_step = find_first_non_done_step(filtered_expected_literal_data)
+    hist_first_non_done_step(non_done_step, "Expected Literal")
+
+    #todo specific warnings (from alerts, etc.)
+
+    #todo specific used commands
+
+    #todo matrice etape premiere erreur / commande
+
+    # Table 
+    fig, ax = plt.subplots()  # Adjust figure size
+    ax.axis("off")  # Hide axes
+    ax.table(cellText=table_data, colLabels=["Number of"], rowLabels=table_headers, bbox=[0.5, 0, 0.5, 1], cellLoc="center", colWidths=[0.5,1])
+    pp.savefig(fig)
+    if show:
+        plt.show()
+
+    pp.close() # Close the pdf file
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
-    input_file = script_dir + "/Outputs/to_keep/llm_extension_with_qa_extension_no_data/QALD-10_sparklisllm_20250303_153759_partiel.json"
+    input_file = script_dir + "/Outputs/to_keep/llm_extension_with_qa_extension_no_data/QALD-10_sparklisllm_20250303_153759.json"
     all_prints(input_file)
