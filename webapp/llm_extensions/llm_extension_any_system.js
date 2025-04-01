f970a60c78c8313b9eac89e1c85eec4540d37e8e
Amice Baptiste <amice.bap@gmail.com>
2025-04-01 11:26:31 +0200

    subquestions scoring references

--- webapp/llm_extensions/llm_extension_any_system.js
+++ webapp/llm_extensions/llm_extension_any_system.js
@@ -612,3 +612,11 @@ class LLMFrameworkBySubquestionsForward extends LLMFramework {
 }
 window.LLMFrameworkBySubquestionsForward = LLMFrameworkBySubquestionsForward;
 window.LLMFrameworks.push(LLMFrameworkBySubquestionsForward.name);
+
+class LLMFrameworkBySubquestionsForwardScoringReferences extends LLMFrameworkBySubquestionsForward {
+    constructor(question, question_id) {
+        super(question, question_id, "count_references");
+    }
+}
+window.LLMFrameworkBySubquestionsForwardScoringReferences = LLMFrameworkBySubquestionsForwardScoringReferences; //to be able to use the class through the window object
+window.LLMFrameworks.push(LLMFrameworkBySubquestionsForwardScoringReferences.name); // to be able to access the class name in the interface and choose it in the dropdown
