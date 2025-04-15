c71803afb394101eca54b1ed51ef83612290ba91
Amice Baptiste <amice.bap@gmail.com>
2025-04-15 18:40:37 +0200

    api format for text2sparql (to test)

--- benchmark/api.py
+++ benchmark/api.py
@@ -17,7 +17,8 @@ app.mount("/static", StaticFiles(directory=script_dir+"/../webapp/"), name="stat
 
 KNOWN_DATASETS = [
     "https://text2sparql.aksw.org/2025/dbpedia/",
-    "https://text2sparql.aksw.org/2025/corporate/"
+    "https://text2sparql.aksw.org/2025/corporate/",
+    "https://query.wikidata.org/sparql"
 ]
 
 @app.get("/")
@@ -27,20 +28,27 @@ async def get_answer(question: str, dataset: str):
     """
     if dataset not in KNOWN_DATASETS:
         raise fastapi.HTTPException(404, "Unknown dataset ...")
+    
+    system_name = "sparklisllm-LLMFrameworkOneShotImproved" #todo change
+    response, error, steps_status, reasoning, driver = interactions.simulated_user(
+        config.SPARKLIS_FILE,
+        lambda driver: interactions.sparklisllm_question(driver, question, dataset, system_name)
+    )
+    driver.close() # should close the page after the api request
     return {
         "dataset": dataset,
         "question": question,
-        "query": "... SPARQL here ..."
+        "query": response,
     }
 
-@app.get("/fetch")
-def fetch_local_page(question: str, endpoint_sparql: str = config.SPARQL_ENDPOINT, system_name: str = "sparklisllm-LLMFrameworkOneShot") -> str:
-    """
-    Draft of the "/" endpoint. #todo
-    """
-    response, error, steps_status, reasoning, driver = interactions.simulated_user(
-        config.SPARKLIS_FILE,
-        lambda driver: interactions.sparklisllm_question(driver, question, endpoint_sparql, system_name)
-    )
-    driver.close() # should close the page after the api request
-    return response
+# @app.get("/fetch")
+# def fetch_local_page(question: str, endpoint_sparql: str = config.SPARQL_ENDPOINT, system_name: str = "sparklisllm-LLMFrameworkOneShot") -> str:
+#     """
+#     Draft of the "/" endpoint. #todo
+#     """
+#     response, error, steps_status, reasoning, driver = interactions.simulated_user(
+#         config.SPARKLIS_FILE,
+#         lambda driver: interactions.sparklisllm_question(driver, question, endpoint_sparql, system_name)
+#     )
+#     driver.close() # should close the page after the api request
+#     return response
