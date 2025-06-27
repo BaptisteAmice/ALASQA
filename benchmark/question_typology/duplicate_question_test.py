951fa655817a57ea67148d53f6db54371a4909f6
Amice Baptiste <amice.bap@gmail.com>
2025-06-27 18:01:49 +0200

    test duplicate question across 2 benchmarks ; could use a pool of example

---
+++ benchmark/question_typology/duplicate_question_test.py
@@ -0,0 +1,48 @@
+import logging
+
+import sys
+import os
+
+# Get the parent directory of the current script
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# Add the parent directory to the system path
+sys.path.insert(0, parent_dir)
+
+# Now you can import modules from the parent directory (not the cleanest way, but works for this case)
+import benchmark_extraction
+
+
+def extract_benchmark(benchmark_file: str, benchmark_name: str, 
+                      language: str = 'en', question_filter: dict = {}) -> list:
+    """
+    Extract benchmark data.
+    """
+    logging.info('Benchmark extraction Start')
+    extractor = benchmark_extraction.extractorFactory(benchmark_name)
+    return extractor.extractData(benchmark_file, language, question_filter)
+
+
+if __name__ == "__main__":
+    # Example usage
+    benchmark_file1 = r'C:\Users\PC\Desktop\llmSparklis\benchmark\Inputs\qald_9_plus_train_dbpedia_patched.json'
+    benchmark_name1 = benchmark_extraction.QALD9_PLUS
+
+    benchmark_file2 = r'C:\Users\PC\Desktop\llmSparklis\benchmark\Inputs\qald_9_plus_test_dbpedia_patched.json'
+    benchmark_name2 = benchmark_extraction.QALD9_PLUS
+
+    # Extract the benchmark data
+    benchmark_data1 = extract_benchmark(benchmark_file1, benchmark_name1)
+    benchmark_data2 = extract_benchmark(benchmark_file2, benchmark_name2)
+
+    # Get the list of questions in both benchmarks
+    questions1 = benchmark_data1[1]
+    questions2 = benchmark_data2[1]
+
+    # Find duplicate questions
+    duplicates = set(questions1) & set(questions2)
+
+    #Print number of duplicates
+    print(f"Number of duplicate questions: {len(duplicates)}")
+    # Print duplicate questions
+    for question in duplicates:
+        print(question)
